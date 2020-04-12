3cc4931c7bc68968ca5d8562cfeb9cdd157edcd7
Mayank Garg <mayank.captain00@gmail.com>
2020-04-13 03:00:27 +0530

    Update remove_errors.py

--- remove_errors.py
+++ remove_errors.py
@@ -2,11 +2,12 @@ def correct_ini_file(config_file):
     with open(config_file, mode='r') as raw_open:
         raw_open.seek(0)
         temp_api_details = raw_open.readlines(0)
-        # print(temp_api_details[0])
+        # print(type(temp_api_details[0]))
 
     with open(config_file, mode='w') as rewrite_config:
-        rewrite_config.write('[TELEGRAM]\n\n')
-        rewrite_config.write(temp_api_details[0])
-        rewrite_config.write(temp_api_details[1] + '\n')
-        rewrite_config.write(temp_api_details[2])
-        rewrite_config.write(temp_api_details[3])
+        if temp_api_details[0] != '[TELEGRAM]\n':
+            rewrite_config.write('[TELEGRAM]\n')
+
+        for i in temp_api_details:
+            rewrite_config.write(i)
+            
